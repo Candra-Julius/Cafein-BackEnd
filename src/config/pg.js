bf845d6beb6e0e6427519391d12bb6bb6dae5bef
Candra Julius <candrajulius97@gmail.com>
2022-06-12 19:22:05 +0700

    add connectionString

--- src/config/pg.js
+++ src/config/pg.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, 
   ssl:{
     rejectUnauthorized:false
   }
