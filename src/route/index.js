9a87f098706d4cfaee7e59fec9c7fd6c78894b71
Candra Julius <candrajulius97@gmail.com>
2022-06-13 08:38:54 +0700

    debug response from auth.js

--- src/route/index.js
+++ src/route/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auth =require('./auth')
+const worker = require('./worker.js')
 
 router
 .use('/auth', auth)
+.use('/users', worker)
 
 module.exports = router
