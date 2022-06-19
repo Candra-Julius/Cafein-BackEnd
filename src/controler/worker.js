867d0ba0382a1a9367f3fdec9a18b5ab8930fc59
Candra Julius <candrajulius97@gmail.com>
2022-06-19 19:32:45 +0700

    debuging getProfile

--- src/controler/worker.js
+++ src/controler/worker.js
@@ -1,295 +1,331 @@
-
-const createError = require('http-errors') 
-const { getProfile, getSkill, getPortofolio, editProfile, addSkill, getWorkExp, addWorkExp, addPortofolio, uploadAva, getALlProfileDefault, getAllProfile, searching, verifySkill, countWorker, editAllProfile } = require('../modul/worker');
-const workerModel = require('../modul/worker');
-const { response } = require('express');
-const  cloudinary = require('cloudinary').v2;
+const createError = require("http-errors");
+const {
+  getProfile,
+  getSkill,
+  getPortofolio,
+  editProfile,
+  addSkill,
+  getWorkExp,
+  addWorkExp,
+  addPortofolio,
+  uploadAva,
+  getALlProfileDefault,
+  getAllProfile,
+  searching,
+  verifySkill,
+  countWorker,
+  editAllProfile,
+} = require("../modul/worker");
+const workerModel = require("../modul/worker");
+const { response } = require("express");
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const workerControl = {
-    getProfile: async(req, res, next) => {
-        try {
-            const id = req.payload.id
-            const {rows: [result]} = await getProfile(id)
-            const {rows: skill} = await getSkill(id)
-            const {rows: port} = await getPortofolio(id)
-            const {rows: work} = await getWorkExp(id)
-            const data = {
-                ...result,
-                skill,
-                port,
-                work
-            }
-            delete data.password
-            res.status(200).json({
-                message: `wellcome ${result.fullname}`,
-                data
-            })
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('internal server error'))
-        }
-    },
-    detailProfile: async(req, res, next) => {
-        try {
-            const id = req.params.id
-            const {rows: [result]} = await getProfile(id)
-            const {rows: skill} = await getSkill(id)
-            const {rows: port} = await getPortofolio(id)
-            const {rows: work} = await getWorkExp(id)
-            const data = {
-                ...result,
-                skill,
-                port,
-                work
-            }
-            delete data.password
-            res.status(200).json({
-                message: `wellcome ${result.fullname}`,
-                data
-            })
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('internal server error'))
-        }
-    },
-    editProfile: async(req, res, next) => {
-        try {
-            const id = req.payload.id
-        console.log(id);
-        const {fullname, jobdesk, address, workplace, description} = req.body
-        const file = req.file
-        if(file){
-            const result = await cloudinary.uploader.upload(req.file.path)
-            const data = {
-                id,
-                fullname,
-                jobdesk,
-                address,
-                workplace,
-                description,
-                image: result.secure_url
-            }
-            await editAllProfile(data)
-        res.status(200).json({
-            data
-        })
-        }else{
-            const data = {
-            id,
-            fullname,
-            jobdesk,
-            address,
-            workplace,
-            description,
-            // image: result.secure_url
-        }
-        console.log(data);
-        await editProfile(data)
-        res.status(200).json({
-            data
-        })
-        }
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('Internal Server Error'))
-        }
-        
-    },
-    addSkill: async(req,res,next) => {
-        try {
-            const id = req.payload.id
-            const {skill} = req.body
-            console.log(id);
-            console.log(skill);
-            const existSkill = await verifySkill(skill, id)
-            console.log(existSkill);
-            if (existSkill.rowCount){
-                res.status(200).json({
-                    message: 'skill already exist'
-                })
-            }else{
-                await addSkill(id, skill)
-        res.status(200).json({
-            message: 'skill added',
-            skill
-        })
-            }
-            
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('Internal Server Error'))
-        }
-        
-    },
-    addWorkExp: async(req, res, next) => {
-        try {
-            const id = req.payload.id
-            const {position, companyname, date, desc} = req.body
-            const data = {
-                id,
-                position,
-                companyname,
-                date,
-                desc
-            }
-            await addWorkExp(data)
-            res.status(200).json({
-                message: 'success',
-                data
-            })
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('Internal Server Error'))
-        }
-    },
-    addPortofolio: async(req, res, next) => {
-        try {
-            const result = await cloudinary.uploader.upload(req.file.path)
-            const id = req.payload.id
-            const {aplicationname, repolink, portotype} = req.body
-            const data = {
-                id,
-                aplicationname,
-                repolink,
-                portotype,
-                img: result.secure_url
-            }
-            await addPortofolio(data)
-            res.status(200).json({
-                message: 'succsess',
-                data
-            })
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('Internal Server Error'))
-        }
-    },
-    // uploadAva: async (req, res, next) =>{
-    //     try {
-    //         const id = req.payload.id
-    //         const result = await cloudinary.uploader.upload(req.file.path)
-    //         const data = {
-    //             id,
-    //             image: result.secure_url
-    //         }
-    //         await uploadAva(data)
-    //         res.status(200).json({
-    //             message: 'success',
-    //             data
-    //         })
-    //     } catch (error) {
-    //         console.log(error);
-    //         next(createError[500]('Internal Server Error'))
-    //     }
-    // },
-    getAllProfile: async (req, res, next) => {
-        try {
-            const page = parseInt(req.query.page) || 1
-            const limit = parseInt(req.query.limit) || 12
-            const offset = (page - 1) * limit
-            const order = req.query.order || 'ASC'
-            const sortby = req.query.sortby
-            const search = req.query.search
-            if (search){
-                const {rows} = await searching(search)
-        const result = rows.map((data)=>data.users_id)
-        console.log(result);
-        const hasil = await Promise.all(result.map( async (data) => {
-                const dataProfile = await workerModel.getProfile(data).then((res) => {return res.rows[0]})
-                // console.log(dataProfile);
-                const dataSkill = await workerModel.getSkill(data).then((res)=> {
-                    delete res.rows.users_id
-                    return res.rows})
-                const val = {
-                    ...dataProfile,
-                    dataSkill
-                }
-                return val
-            })) 
-            const { rows: [count] } = await countWorker()
-            const totalData = parseInt(count.total)
-            totalPage = Math.ceil(totalData / limit)
-            const pagination = {
-                currentPage: page,
-                limit,
-                totalData,
-                totalPage,
-            }
-        res.status(200).json({
-            message: 'success',
-            pagination,
-            hasil
-        })
-            }else {
-                if (sortby){
-                const {rows} = await getAllProfile(sortby, order, limit, offset)
-                const ids = rows.map((data)=> data.iduser)
-                const result = await Promise.all(ids.map(async(data)=>{
-                    const dataProfile = await getAllProfile(sortby, order, limit, offset).then((res)=>{return res.rows[0]})
-                    const dataSkill = await workerModel.getSkill(data).then((res)=> {
-                        return res.rows})
-                        const val = {
-                            ...dataProfile,
-                            dataSkill
-                        }
-                        return val
-                }))
-                const { rows: [count] } = await countWorker()
-            const totalData = parseInt(count.total)
-            totalPage = Math.ceil(totalData / limit)
-            const pagination = {
-                currentPage: page,
-                limit,
-                totalData,
-                totalPage,
-            }
-        res.status(200).json({
-            message: 'success',
-            pagination,
-            hasil
-        })
-            }else {
-                const {rows} = await getALlProfileDefault(limit, offset)
-                const ids = rows.map((data)=> data.iduser)
-                const hasil = await Promise.all(ids.map(async(data)=>{
-                    const dataProfile = await getALlProfileDefault(limit, offset).then((res) => {return res.rows[0]})
-                    const dataSkill = await workerModel.getSkill(data).then((res)=> {
-                        return res.rows})
-                        // console.log(dataProfile);
-                        const val = {
-                            ...dataProfile,
-                            dataSkill
-                        }
-                        console.log(val);
-                        return val
-                }))
-                const { rows: [count] } = await countWorker()
-            const totalData = parseInt(count.total)
-            totalPage = Math.ceil(totalData / limit)
-            const pagination = {
-                currentPage: page,
-                limit,
-                totalData,
-                totalPage,
-            }
-        res.status(200).json({
-            message: 'success',
-            pagination,
-            hasil
-        })
-            }
-            }
-            
-        } catch (error) {
-            console.log(error);
-            next(createError[500]('Internal Server Error'))
-        }
+  getProfile: async (req, res, next) => {
+    try {
+      const id = req.payload.id;
+      const {
+        rows: [result],
+      } = await getProfile(id);
+      const { rows: skill } = await getSkill(id);
+      const { rows: port } = await getPortofolio(id);
+      const { rows: work } = await getWorkExp(id);
+      const data = {
+        ...result,
+        skill,
+        port,
+        work,
+      };
+      delete data.password;
+      res.status(200).json({
+        message: `wellcome ${result.fullname}`,
+        data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("internal server error"));
     }
-}
+  },
+  detailProfile: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const {
+        rows: [result],
+      } = await getProfile(id);
+      const { rows: skill } = await getSkill(id);
+      const { rows: port } = await getPortofolio(id);
+      const { rows: work } = await getWorkExp(id);
+      const data = {
+        ...result,
+        skill,
+        port,
+        work,
+      };
+      delete data.password;
+      res.status(200).json({
+        message: `wellcome ${result.fullname}`,
+        data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("internal server error"));
+    }
+  },
+  editProfile: async (req, res, next) => {
+    try {
+      const id = req.payload.id;
+      console.log(id);
+      const { fullname, jobdesk, address, workplace, description } = req.body;
+      const file = req.file;
+      if (file) {
+        const result = await cloudinary.uploader.upload(req.file.path);
+        const data = {
+          id,
+          fullname,
+          jobdesk,
+          address,
+          workplace,
+          description,
+          image: result.secure_url,
+        };
+        await editAllProfile(data);
+        res.status(200).json({
+          data,
+        });
+      } else {
+        const data = {
+          id,
+          fullname,
+          jobdesk,
+          address,
+          workplace,
+          description,
+          // image: result.secure_url
+        };
+        console.log(data);
+        await editProfile(data);
+        res.status(200).json({
+          data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("Internal Server Error"));
+    }
+  },
+  addSkill: async (req, res, next) => {
+    try {
+      const id = req.payload.id;
+      const { skill } = req.body;
+      console.log(id);
+      console.log(skill);
+      const existSkill = await verifySkill(skill, id);
+      console.log(existSkill);
+      if (existSkill.rowCount) {
+        res.status(200).json({
+          message: "skill already exist",
+        });
+      } else {
+        await addSkill(id, skill);
+        res.status(200).json({
+          message: "skill added",
+          skill,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("Internal Server Error"));
+    }
+  },
+  addWorkExp: async (req, res, next) => {
+    try {
+      const id = req.payload.id;
+      const { position, companyname, date, desc } = req.body;
+      const data = {
+        id,
+        position,
+        companyname,
+        date,
+        desc,
+      };
+      await addWorkExp(data);
+      res.status(200).json({
+        message: "success",
+        data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("Internal Server Error"));
+    }
+  },
+  addPortofolio: async (req, res, next) => {
+    try {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      const id = req.payload.id;
+      const { aplicationname, repolink, portotype } = req.body;
+      const data = {
+        id,
+        aplicationname,
+        repolink,
+        portotype,
+        img: result.secure_url,
+      };
+      await addPortofolio(data);
+      res.status(200).json({
+        message: "succsess",
+        data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("Internal Server Error"));
+    }
+  },
+  getAllProfile: async (req, res, next) => {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 12;
+      const offset = (page - 1) * limit;
+      const order = req.query.order || "ASC";
+      const sortby = req.query.sortby;
+      const search = req.query.search;
+      if (search) {
+        const { rows } = await searching(search);
+        const ids = rows.map((data) => data.users_id);
+        const data = await Promise.all(ids.map(async(datas) => {
+            return (profile = await getProfile(datas).then((res) => {
+                return res.rows
+            }))
+        }))
+        const hasil = await Promise.all(
+            ids.map(async (data) => {
+              return ([dataSkill] = await workerModel
+                .getSkill(data)
+                .then((res) => {
+                  return res.rows;
+                }));
+            })
+          );
+          let datas;
+          let skill = {};
+          let val = [];
+          for (let i = 0; i < data.length; i++) {
+            datas = data[i];
+            skill = hasil[i].map((item) => item.skillname);
+            val.push({
+              ...datas[i],
+              skill,
+            });
+          }
+          console.log(val);
+          const {
+            rows: [count],
+          } = await countWorker();
+          const totalData = parseInt(count.total);
+          totalPage = Math.ceil(totalData / limit);
+          const pagination = {
+            currentPage: page,
+            limit,
+            totalData,
+            totalPage,
+          };
+          res.status(200).json({
+            message: "success",
+            pagination,
+            val,
+          });
+      } else {
+        if (sortby) {
+          const { rows } = await getAllProfile(sortby, order, limit, offset);
+          const ids = rows.map((data) => data.iduser);
+          const hasil = await Promise.all(
+            ids.map(async (data) => {
+              return ([dataSkill] = await workerModel
+                .getSkill(data)
+                .then((res) => {
+                  return res.rows;
+                }));
+            })
+          );
+          let datas;
+          let skill = {};
+          let val = [];
+          for (let i = 0; i < rows.length; i++) {
+            datas = rows[i];
+            skill = hasil[i].map((item) => item.skillname);
+            val.push({
+              ...datas,
+              skill,
+            });
+          }
+          console.log(val);
+          const {
+            rows: [count],
+          } = await countWorker();
+          const totalData = parseInt(count.total);
+          totalPage = Math.ceil(totalData / limit);
+          const pagination = {
+            currentPage: page,
+            limit,
+            totalData,
+            totalPage,
+          };
+          res.status(200).json({
+            message: "success",
+            pagination,
+            val,
+          });
+        } else {
+          const { rows: data } = await getALlProfileDefault(limit, offset);
+          const ids = data.map((data) => data.iduser);
+          const hasil = await Promise.all(
+            ids.map(async (data) => {
+              return ([dataSkill] = await workerModel
+                .getSkill(data)
+                .then((res) => {
+                  return res.rows;
+                }));
+            })
+          );
+          let datas;
+          let skill = {};
+          let val = [];
+          for (let i = 0; i < data.length; i++) {
+            datas = data[i];
+            skill = hasil[i].map((item) => item.skillname);
+            val.push({
+              ...datas,
+              skill,
+            });
+          }
+          console.log(val);
+          const {
+            rows: [count],
+          } = await countWorker();
+          const totalData = parseInt(count.total);
+          totalPage = Math.ceil(totalData / limit);
+          const pagination = {
+            currentPage: page,
+            limit,
+            totalData,
+            totalPage,
+          };
+          res.status(200).json({
+            message: "success",
+            pagination,
+            val,
+          });
+        }
+        }
+    } catch (error) {
+      console.log(error);
+      next(createError[500]("Internal Server Error"));
+    }
+  },
+};
 
 module.exports = workerControl;
--- src/modul/worker.js
+++ src/modul/worker.js
@@ -11,7 +11,7 @@ const workerModel = {
         return pool.query('UPDATE users SET fullname = $1, jobdesk = $2, address = $3, workplace = $4, description = $5, profileimage = $6 WHERE iduser = $7', [data.fullname, data.jobdesk, data.address, data.workplace, data.description, data.image, data.id])
     },
     getSkill: (id) => {
-        return pool.query('SELECT * FROM skill WHERE users_Id = $1', [id])
+        return pool.query('SELECT skillname FROM skill WHERE users_Id = $1', [id])
     },
     addSkill: (id, skill) => {
         return pool.query('INSERT INTO skill (skillname, users_id) VALUES ($1, $2)',[skill, id])
