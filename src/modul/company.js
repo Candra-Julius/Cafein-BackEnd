28df8e7df785cea5d4018a58c4600b5404c9626a
Irfan Abidin <irfan43abdin@gmail.com>
2022-07-21 10:26:02 +0700

    edit copmany foto

--- src/modul/company.js
+++ src/modul/company.js
@@ -6,7 +6,16 @@ const companyModel = {
   },
   editProfile: (data) => {
     return pool.query(
-      `UPDATE company SET companyname = COALESCE ($1,companyname), jobfield = COALESCE ($2,jobfield), companyaddress = COALESCE ($3,companyaddress), description = COALESCE ($4,description), emailcompany = COALESCE ($5,emailcompany),instagram = COALESCE ($6,instagram),companyphone = COALESCE ($7,companyphone),linkedin = COALESCE ($8,linkedin),profileimage = COALESCE ($9,profileimage) WHERE idcompany = $10`,
+      `UPDATE company SET 
+      companyname = COALESCE($1,companyname),
+      jobfield = COALESCE($2,jobfield),
+      companyaddress = COALESCE($3,companyaddress),
+      description = COALESCE($4,description),
+      emailcompany = COALESCE($5,emailcompany),
+      instagram = COALESCE($6,instagram),
+      companyphone = COALESCE($7,companyphone),
+      linkedin = COALESCE($8,linkedin),
+      profileimage = COALESCE($9,profileimage) WHERE idcompany = $10`,
       [data.company, data.jobfield, data.address, data.description, data.email, data.instagram, data.phone, data.linkedin, data.image, data.id]
     );
   },
